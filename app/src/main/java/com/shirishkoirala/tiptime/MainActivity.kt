89f0dbdfb43e3af02272ffd82555f10ff3e162cc
Shirish Koirala <shirishkoirala@gmail.com>
2022-07-18 14:00:40 +1000

    Create a more polished user experince complete.

--- app/src/main/java/com/shirishkoirala/tiptime/MainActivity.kt
+++ app/src/main/java/com/shirishkoirala/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.shirishkoirala.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.shirishkoirala.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener{calculateTip()}
+
+        binding.costOfService.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
     }
 
     private fun calculateTip(){
@@ -36,7 +42,14 @@ class MainActivity : AppCompatActivity() {
         }else{
             binding.tipResult.text = "Invalid Value"
         }
-
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int):Boolean{
+        if(keyCode == KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
     private fun displayTip(tip: Double){
